829b374a01454fca7d781eed81ab1b3b0dfafd38
Ed <saeedmuhaisen97@gmail.com>
2024-02-01 06:15:18 +0300

    initial spring boot application

---
+++ src/main/java/com/HarmonyTracker/HarmonyTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.HarmonyTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HarmonyTracker/HarmonyTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HarmonyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
