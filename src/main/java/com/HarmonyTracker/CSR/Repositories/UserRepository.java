47b4f1ddf014b2ef23f04b427cc937951d1b23f4
Ed <saeedmuhaisen97@gmail.com>
2024-02-05 03:54:46 +0300

    checkpoint for authentication

--- src/main/java/com/HarmonyTracker/CSR/Repositories/UserRepository.java
+++ src/main/java/com/HarmonyTracker/CSR/Repositories/UserRepository.java
@@ -3,12 +3,11 @@ package com.HarmonyTracker.CSR.Repositories;
 import com.HarmonyTracker.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
-    Optional<User> findByFacebookId(String facebookId);
-    Optional<User> findByAppleId(String appleId);
+    Optional<User> findByOauthId(String oauthId);
 }
